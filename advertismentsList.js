7af196b4fb9a91aa877bc8a5f8f28d1333dfdc0f
Josep Valls <alfredvays@gmail.com>
2022-09-24 20:51:31 +0200

    Base de datos sparrest.js conectada con fetch

--- advertismentsList.js
+++ advertismentsList.js
@@ -1,42 +1,13 @@
 
-export function getAdvertisments(){
+export async function getAdvertisments(){
 
+    const advertismentsURL = 'http://localhost:8000/api/users'
 
-    return new Promise((resolve,reject) => {
-
-        setTimeout(() => {
-            const advertismentsList = [
-                {
-                    statement: 'Wanted',
-                    photo: 'https://img.remediosdigitales.com/109c49/volkswagen-beetle-1967-annie-1-/450_1000.jpg',
-                    name: 'VolksWagen Beetle',
-                    description: 'string',
-                    price: 'max. 5000 €'
-                },
-                {
-                    statement: 'For Sale',
-                    photo: 'https://violin.com',
-                    name: 'Stradivarius',
-                    description: 'Violin de la casa Stradivarius',
-                    price: '3000 €'
-                },
-                {
-                    statement: 'For Sale',
-                    photo: 'https://sneakers.com',
-                    name: 'Adidas',
-                    description: 'string',
-                    price: '40 €'
-                },
-                {
-                    statement: 'Wanted',
-                    photo: 'https://table.com',
-                    name: 'Student table',
-                    description: 'string',
-                    price: 'max. 150 €'
-                },
-            ];
-
-            resolve(advertismentsList)
-        },1500)
+    return new Promise(async (resolve,reject) => {
+      //Hacemos fetch a la URL para conectarnos 
+      const response = await  fetch(advertismentsURL);
+      //Recibimos los datos
+      const advertisments = await response.json()
+      resolve(advertisments)
     });
-}
+};

50fcc55c2874b7b1bacc824cf6a30375e7067171
Josep Valls <alfredvays@gmail.com>
2022-10-01 20:20:14 +0200

    creo adsDetail.html

--- ads__list/adsView.js
+++ ads__list/adsView.js
@@ -6,11 +6,13 @@
  */
 export const buildAdView = (ad) => {
     const adView = `
+    <a href="http://localhost:8080/adsDetail.html?id=${ad.id}">
     <h2 class="ad__statement">${ad.statement}</h2>
     <h2 class="ad__name">${ad.name}</h2>
     <img class="ad__photo" src="${ad.photo}" alt="${ad.name}">
     <h3 class="ad__description">${ad.description}</h3>
     <h2 class="ad__price">${ad.price}</h2>
+   </a> 
     `
     return adView
 };
